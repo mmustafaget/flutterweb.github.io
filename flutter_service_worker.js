2c9f8557aaa6026bea46c3b77f4c04367dba3d55
mmustafaget <mustafa@get-licensed.co.uk>
2021-07-29 00:08:04 +0500

    update loader

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "fb2affb87beb512babbbf63c7881346b",
-"index.html": "c4cbdb3218a0caed4ac7e08b0a4bc06e",
-"/": "c4cbdb3218a0caed4ac7e08b0a4bc06e",
-"main.dart.js": "0a758db7087236e49f9b6aeea0355286",
+"index.html": "6993bc7d7709a328c6d185238064d0fe",
+"/": "6993bc7d7709a328c6d185238064d0fe",
+"main.dart.js": "409332dbe8d3fad78b86e84cc2a6ed0f",
 "icons/favicon-16x16.png": "25210f4bb8095faebd2cc4cd609e41b0",
 "icons/favicon.ico": "f5c3d2e85be33dfe812ff3dd51bdfed6",
 "icons/apple-icon.png": "913b498ea515de7cd056992e8f48e71b",
